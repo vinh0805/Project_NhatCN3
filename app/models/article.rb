b3cf415eb472ac964597a16d43fb7f660498290b
Ubuntu <ubuntu@ip-172-31-64-247.ec2.internal>
2020-12-01 14:19:31 +0000

    some change

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
+  # mot article chi thuoc ve mot user
   belongs_to :user
-  has_many :article_categories
-  has_many :categories, through: :article_categories
+
+  has_many  :article_categories
+  has_many  :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
